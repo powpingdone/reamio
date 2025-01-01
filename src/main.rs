bd158a6cd941115fd78346ab6a24c75d00725900
Aidan <aidanzcase@gmail.com>
2025-01-01 13:41:48 -0600

    Add base template

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use axum::Router;
+use std::sync::{Arc, Weak};
+
+use axum::{
+    extract::State,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use minijinja::Environment;
 use sqlx::{
     sqlite::{SqliteConnectOptions, SqlitePoolOptions},
@@ -8,7 +15,27 @@ use sqlx::{
 #[derive(Clone)]
 pub struct ReamioApp<'a> {
     pub user_db: SqlitePool,
-    pub jinja: Environment<'a>,
+    pub jinja: Weak<Environment<'a>>,
+}
+
+async fn main_page<'a>(State(state): State<ReamioApp<'a>>) -> impl IntoResponse {
+    Html(
+        state
+            .jinja
+            .upgrade()
+            .unwrap()
+            .get_template("base.html")
+            .unwrap()
+            .render(minijinja::context!())
+            .unwrap(),
+    )
+}
+
+fn load_templates() -> Arc<Environment<'static>> {
+    let mut ret = Environment::new();
+    ret.add_template("base.html", include_str!("templates/base.html.jinja"))
+        .unwrap();
+    Arc::new(ret)
 }
 
 #[tokio::main]
@@ -21,12 +48,12 @@ async fn main() {
         )
         .await
         .unwrap();
-    let jinja = Environment::new();
+    let jinja = load_templates();
     let state = ReamioApp {
         user_db,
-        jinja,
+        jinja: Arc::downgrade(&jinja),
     };
-    let router = Router::new().with_state(state);
+    let router = Router::new().route("/", get(main_page)).with_state(state);
     axum::serve(
         tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap(),
         router,
