df4a3ebbaae84e5c59c9df2e311933eac8137feb
Aidan <aidanzcase@gmail.com>
2025-05-24 11:23:01 -0500

    Some stuff

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -2,7 +2,15 @@ slint::include_modules!();
 
 fn main() {
     let main_window = MainWindow::new().unwrap();
+    let w_main_window = main_window.weak();
+
+    std::thread::spawn(move || {
+        let get = ureq::get("http://localhost:8080/api/tabledump")
+            .call()
+            .unwrap()
+            .json::<>();
+    })
+    .unwrap();
 
     main_window.run().unwrap();
 }
-
